6c48c73103788443ae507f969482e6e39f5a9a5d
Nathan Loding <nathan@withoutthea.com>
2013-03-24 11:55:04 -0400

    Nearly done with static front page, starting on individual page styles to compliment.

--- footer.php
+++ footer.php
@@ -1,20 +1,23 @@
 </section><!-- Container End -->
 
-<div class="row full-width">
+<!-- <div class="row full-width footer"> -->
+<div class="footer opacity-90">
+    <div class="row full-width">
 	<?php dynamic_sidebar("Footer"); ?>
+    </div>
+
+    <footer class="row full-width" role="contentinfo">
+        <div class="small-12 large-4 columns">
+            <p>&copy; <?php echo date('Y'); ?>. Crafted on <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
+        </div>
+
+        <div class="small-12 large-8 columns">
+            <?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list right')); ?>
+        </div>
+    </footer>
+
+    <?php wp_footer(); ?>
 </div>
-
-<footer class="row full-width" role="contentinfo">
-	<div class="small-12 large-4 columns">
-		<p>&copy; <?php echo date('Y'); ?>. Crafted on <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
-	</div>
-	
-	<div class="small-12 large-8 columns">
-		<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list right')); ?>
-	</div>
-</footer>
-
-<?php wp_footer(); ?>
 
 <script>
 	$(document).foundation();
--- template_index.php
+++ template_index.php
@@ -8,7 +8,7 @@ get_header(); ?>
 	<div class="small-12 large-12 columns" role="main">
         
         <div class="row">
-            <div class="hide-for-small large-12 colums">
+            <div class="hide-for-small large-12 colums wota-bottom-border">
                 <ul data-orbit>
                     <li>
                         <img src="http://faeyephotography.com/portfolio/weddings/engagement/01engagement.jpg" />
@@ -28,8 +28,9 @@ get_header(); ?>
                     </li>
                 </ul>
             </div>
+        </div>
         
-        <div class="row">
+        <div class="row wota-pad-the-top wota-bottom-border">
             <div class="large-4 columns">
                 <div class="panel">Testing panel #1.</div>
             </div>
@@ -40,24 +41,18 @@ get_header(); ?>
                 <div class="panel">Testing panel #3.</div>
             </div>
         </div>
-	
-	<?php /* Start loop */ ?>
-	<?php while (have_posts()) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php reverie_entry_meta(); ?>
-			</header>
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div>
-			<footer>
-				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
-				<p><?php the_tags(); ?></p>
-			</footer>
-			<?php comments_template(); ?>
-		</article>
-	<?php endwhile; // End the loop ?>
+            
+        <div class="row wota-pad-the-top">
+            <div class="large-3 columns">
+                Testing panel #1.
+            </div>
+            <div class="large-6 columns wota-border-left">
+                Testing panel #2.
+            </div>
+            <div class="large-3 columns wota-border-left">
+                Testing panel #3.
+            </div>
+        </div>
 
 	</div>
 		
